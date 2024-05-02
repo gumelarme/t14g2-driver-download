c2df9d4f96324bf6f9f375aa7ca46e89202cb006
gumelarme <gumelar.pn@gmail.com>
2024-05-03 01:32:37 +0800

    feat: working download mechanism

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
 import httpx
 import json
+import logging
+
+from pathlib import Path
+
+DOWNLOAD_DIR = "downloads"
 
 def get_file_list(filename: str) -> list[dict[str, any]]:
     with open(filename, 'r') as f:
@@ -7,10 +13,28 @@ def get_file_list(filename: str) -> list[dict[str, any]]:
 
 
 def main():
+    Path(DOWNLOAD_DIR).mkdir(parents=True, exist_ok=True)
+
     files = get_file_list("driver-list.json")
     for file in files:
-        print(file["name"])
+        filename = os.path.join(DOWNLOAD_DIR, file["name"])
+        logging.info(f"Downloading {file['link']}")
+        try:
+            download_file(filename, file["link"])
+        except httpx.HTTPError:
+            logging.error(f"Failed downloading {filename}")
+            continue
+
+
+def download_file(filename: str, link: str):
+    with httpx.stream("GET", link) as r:
+        r.raise_for_status()
+
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_bytes(chunk_size=2 ** 13):
+                f.write(chunk)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
 
